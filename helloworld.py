99487ad00bbe6e00c40645a3dff6a5af25503fb3
GabrielRF <github@gabrf.com>
2017-09-06 15:14:10 -0300

    Removed commands. Added subprocess

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,5 @@
 import telebot
-import commands
+import subprocess
 import os
 
 token = ''
@@ -11,19 +11,20 @@ def send_message(text):
     bot.send_message(dest, text)
 
 def get_ip():
-    output = commands.getoutput("ip addr | grep wlan0 | grep inet")
-    if '255' not in output:
+    cmd = "ip addr | grep wlan0 | grep inet"
+    output = subprocess.check_output(cmd, shell=True)
+    if '255' not in str(output):
         return 0
-    return output
+    return output.decode('utf-8')
 
 def get_hostname():
-    output = commands.getoutput("hostname")
-    return output
+    output = subprocess.check_output("hostname")
+    return output.decode('utf-8')
 
 if __name__ == '__main__':
     ip = get_ip()
     while ip == 0:
         ip = get_ip()
     hostname = get_hostname()
-    message = (str(hostname) + '\n' + str(ip))
+    message = (str(hostname) + str(ip))
     send_message(message)
